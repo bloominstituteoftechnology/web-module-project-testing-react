13eb8a9585190e9e94a851588ee91b8f2715fcc7
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-22 12:13:13 -0700

    first commit

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,19 +8,34 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
-    runtime: 1
+    summary: "Panz wuz here!",
+    runtime: 1,
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id: 1,
+    name:'',
+    season: 1,
+    number:1,
+    summary:'Hi yall', 
+    runtime:1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode} />);
 });
 
 test("renders the summury test passed as prop", ()=>{
+    //arrange
+    render(<Episode episode={testEpisode} />)
+
+    //act
+    const summary = screen.queryByText("Panz wuz here!")
+
+    //assert
+    expect(summary).toBeInTheDocument();
+    expect(summary).not.toBeNull();
     
 });
 
