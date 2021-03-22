d1f0f08e23ebbc6c76813630eb06ea603f64a139
ClintFix <clint.fix@gmail.com>
2021-03-22 13:59:01 -0600

    more tests in show component complete

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -21,11 +21,16 @@ test('renders testShow and no selected Season without errors', ()=>{
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} selectedSeason={'none'} />)
+    const loading = screen.queryByTestId('loading-container');
+    expect(loading).toBeInTheDocument();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'} />)
+    const seasonOptions = screen.queryAllByTestId('season-option')
+    expect(seasonOptions).toHaveLength(4);
 });
-
 test('handleSelect is called when an season is selected', () => {
 });
 
