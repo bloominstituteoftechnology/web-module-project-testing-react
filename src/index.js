bfa1d2e872a3e3b79099bb329d2404155d4eb23c
GregoryPWalton <75237506+GregoryPWalton@users.noreply.github.com>
2021-04-19 20:55:35 -0700

    gregory-walton

--- src/index.js
+++ src/index.js
@@ -5,3 +5,44 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,44 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
+
+ 
+  
+  
