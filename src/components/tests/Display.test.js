80df5293c603947ee3cc9803de33fa7c7c4a1ddc
Devon Osei <oseidevon@gmail.com>
2021-04-19 22:16:29 -0400

    yo

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,6 +1,9 @@
-
-
-
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Display from './../Display';
+import mockFetchShow from '../../api/fetchShow'
+jest.mock
 
 
 
