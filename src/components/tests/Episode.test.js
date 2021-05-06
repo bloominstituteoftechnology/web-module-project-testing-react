a2577507cc0fea2c0ab8c32446cc89149e9ac678
orlando taylor <ackerie_taylor@hotmail.com>
2021-05-06 00:06:04 -0400

    work to be done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { getByText, render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 const testEpisode = {
@@ -14,14 +14,24 @@ const testEpisode = {
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+render(<Episode episode={testEpisode}/>)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode={testEpisode}/>)
+    const summary = getByText(/text/i)
+
+    expect(summary).toBeTruthy()
 });
 
 test("renders default image when image is not defined", ()=>{
