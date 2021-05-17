eb35e2d6347605715846d31905ac6b1251adefdc
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-05-17 14:32:48 -0400

    MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,4 +1,53 @@
+import React from 'react'; 
+import {render, creen, waitfor} from '@testing-libary/react';
+import userEvent from '@testing-libary/user-event';
 
+import Display from './../Display';
+
+const testShow = {
+    name: "Yogi Bear",
+    summary:"A bear named Yogi along with his friend Boo-Boo try to steal forest visiter's picnic baskets.",
+    seasons:[
+        {id:0, name:"Season 1", episodes: []}
+    ]
+}
+
+const displayFunc = (data) =>{
+    console.log(data);
+}
+
+test('renders without props', ()=>{
+    render(<Display/>)
+});
+
+test('test show component showing', ()=>{
+    render(<Display displayFunc = {displayFunc}/>)
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = screen.queryByTestId('show-container');
+    waitfor(()=> expect(show).toBeInTheDocument());
+});
+
+test('test operations rendered equals seasons',()=>{
+    render (<Display show = {testShow}/>);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    waitfor(()=> expect(screen.getAllByTestId('season-option')).toHaveLength(testShow.seasons.length));
+});
+
+test('test if optional function is being called', ()=>{
+    const fakeClick = jest.fn();
+    render(<Display handleClick = {fakeClick}/>);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    waitfor(()=> expect(fakeClick).toHaveBeenCalledTimes(1));
+});
 
 
 
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,23 +8,42 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "This is a summary",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name:'Episode 1',
+    img:'',
+    season: 1,
+    number:1,
+    summary:'none',
+    runtime:1
 }
 
 test("renders without error", () => {
+    render(<Episode episode = {[]}/>)
 
 });
 
 test("renders the summury test passed as prop", ()=>{
+    render(<Episode episode = {testEpisode}/>)
+    let summary = screen.getByText(`${testEpisode.summary}`)
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(testEpisode.summary);
+
     
 });
 
 test("renders default image when image is not defined", ()=>{
+    render(<Episode episode = {testEpisodeWithoutImage}/>)
+    let img = document.getElementsByTagName('img')[0];
+    expect(img).toBeInTheDocument();
+    expect(img.alt).toEqual("./stranger_things.png");
+
     
 })
 
