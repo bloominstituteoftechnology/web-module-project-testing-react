a7419a4604aa4eb3c215e284ec044e2ab9f60801
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-17 18:05:39 -0400

    Show component renders with testShow data and selected season prop set to none

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  //add in approprate test data structure here.
+  name: "",
+  summury: "",
+  seasons: [
+    {
+      id: Date.now(),
+      name: "",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show props={{ show: testShow, selectedSeason: "none" }} />);
 });
 
-test('renders Loading component when prop show is null', () => {
-});
+test("renders Loading component when prop show is null", () => {});
 
-test('renders same number of options seasons are passed in', ()=>{
-});
+test("renders same number of options seasons are passed in", () => {});
 
-test('handleSelect is called when an season is selected', () => {
-});
+test("handleSelect is called when an season is selected", () => {});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
-});
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
