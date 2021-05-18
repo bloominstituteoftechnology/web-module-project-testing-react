2edea1dccbdb0718afcd0a4ff8bbcc15166ed037
Alex Edwards <alexedwards@alexs-mbp.lan>
2021-05-17 22:10:13 -0600

    mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,20 +1,61 @@
+import React from "react";
+import { screen, render, waitFor } from "@testing-library/react";
+import Display from "../Display";
+import mockFetchShow from "../../api/fetchShow";
+import userEvent from "@testing-library/user-event";
+jest.mock("../../api/fetchShow");
 
+const testShow = {
+  //add in approprate test data structure here.
+  name: "Stranger Things",
+  summary:
+    "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+    { id: 2, name: "Season 3", episodes: [] },
+    { id: 3, name: "Season 4", episodes: [] },
+  ],
+};
 
+test("renders without error", () => {
+  render(<Display />);
+});
 
+test("when the fetch button is pressed, the show component will display", async () => {
+  render(<Display />);
+  mockFetchShow.mockResolvedValueOnce(testShow);
 
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
 
+  const showDetails = await screen.findByTestId("show-container");
+  expect(showDetails).toBeInTheDocument();
+});
 
+test("when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in  test data", async () => {
+  render(<Display />);
+  mockFetchShow.mockResolvedValueOnce(testShow);
 
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
 
+  const seasons = await screen.findAllByTestId("season-option");
+  expect(seasons).toHaveLength(4);
+});
 
+test("when the fetch button is pressed, this displayFunc function is called", async () => {
+  const mockDisplayFunc = jest.fn();
+  render(<Display displayFunc={mockDisplayFunc} />);
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  const button = screen.queryByRole("button");
+  userEvent.click(button);
 
-
-
-
-
+  await waitFor(() => expect(mockDisplayFunc).toHaveBeenCalled());
+});
 
 ///Tasks:
-//1. Add in nessisary imports and values to establish the testing suite.
+//1. Add in necessary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  //add in approprate test data structure here.
+  name: "Stranger Things",
+  summary: "A love letter",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+    { id: 2, name: "Season 3", episodes: [] },
+    { id: 3, name: "Season 4", episodes: [] },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
 });
 
-test('renders Loading component when prop show is null', () => {
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} selectedSeason={"none"} />);
+  const loading = screen.queryByTestId("loading-container");
+  expect(loading).toBeInTheDocument();
 });
 
-test('renders same number of options seasons are passed in', ()=>{
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+  const seasons = screen.queryAllByTestId(/season-option/i);
+  expect(seasons.length).toBe(4);
 });
 
-test('handleSelect is called when an season is selected', () => {
+test("handleSelect is called when an season is selected", () => {
+  const mockSeason = jest.fn();
+
+  render(
+    <Show handleSelect={mockSeason} show={testShow} selectedSeason={"none"} />
+  );
+  userEvent.selectOptions(screen.getByLabelText("Select A Season"), ["1"]);
+  expect(mockSeason).toHaveBeenCalled();
 });
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {
+  const mockSeason = jest.fn();
+
+    const {rerender} = render(<Show handleSelect={mockSeason} show={testShow} selectedSeason={'none'} />)
+    let episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).not.toBeInTheDocument();
+
+
+    rerender(<Show handleSelect={mockSeason} show={testShow} selectedSeason={1} />)
+    episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).toBeInTheDocument();
+
 });
 
 //Tasks:
