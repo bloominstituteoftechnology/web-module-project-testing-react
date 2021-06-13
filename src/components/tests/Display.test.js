5a996d270620aeccd3c7a5d8d79bb5fc4c200ad7
Matthew Larson <maflarson@gmail.com>
2021-06-13 13:36:50 -0600

    mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,18 +1,50 @@
+import React from 'react';
+import { render, screen, waitfor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Display from '../Display'
 
 
+const testDisplay = {
+    name: "Stranger Things",
+    summary:"A town in the 80's thats more than it seems.",
+    seasons:[
+        {id:0, name:"Season 2", episodes: []}
+    ]
+}
+const displayFunction = (data) => {
+    console.log(data);
+}
 
+test('renders without any props', () => {
+    render(<Display/>)
+});
 
+test('test component showing', () => {
+    render(<Display displayFunction = {displayFunction}/>)
 
+    const show = screen.queryByTestId('show-container');
+    waitfor( () => expect(show).toBeInTheDocument());
+    
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+});
 
+test('test seasons to have right # of ep',() => {
+    render (<Display show = {testDisplay}/>);
 
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    waitfor( () => expect(screen.getAllByTestId('season-option')).toHaveLength(testDisplay.seasons.length));
+});
 
+test('test if optional function is being called', () => {
+    const mockClick = jest.fn();
 
-
-
-
-
-
-
+render(<Display handleClick = {mockClick}/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    waitfor( () => expect(mockClick).toHaveBeenCalledTimes(1));
+})
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,30 +4,47 @@ import Episode from './../Episode';
 
 const testEpisode = {
     id:1,
-    name: "",
+    name: "Stranger things",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "summarysummarysummary",
     runtime: 1
 }
 
-const testEpisodeWithoutImage = {
+const testEpisodeWithoutImage = {id:1,
+    name:'Episode 5',
+    img:'',
+    season: 2,
+    number:5,
+    summary:'words in summary',
+    runtime:1
     //Add in approprate test data structure here.
 }
 
 test("renders without error", () => {
-
+render(<Episode episode = {[]} />)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+render(<Episode episode = {testEpisode}/>)
+
+let summary = screen.getByText(`${testEpisode.summary}`)
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(testEpisode.summary);
+    expect(summary).toBeTruthy();
 });
 
 test("renders default image when image is not defined", ()=>{
-    
-})
+    render(<Episode episode={testEpisodeWithoutImage} />);
 
+    const info = screen.getByAltText('./stranger_things.png');
+
+    expect(info).toBeInTheDocument();
+    expect(info.alt).toEqual('./stranger_things.png');
+    expect(info).toBeTruthy();
+})
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
 //2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use no more then 3 different expect statements to test the the existance of the summary value.
