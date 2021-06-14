5bceb5a213a6c586ac1ab286dcd812912478a606
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-14 14:22:49 -0500

    tests 1 & 2 working for episode component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,7 +8,7 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "What a thriller of an episode",
     runtime: 1
 }
 
@@ -24,11 +24,15 @@ const testEpisodeWithoutImage = {
 }
 
 test("renders without error", () => {
-    // render(<Episode />);
+    render(<Episode  episode={testEpisode}/>);
 });
 
 test("renders the summary test passed as prop", ()=>{
-    
+    render(<Episode  episode={testEpisode}/>);
+    const summary = screen.queryByText(/what a thriller of an episode/i);
+    expect(summary).toBeTruthy();
+    expect(summary).toBeInTheDocument();
+    expect(summary).not.toBeFalsy();
 });
 
 test("renders default image when image is not defined", ()=>{
@@ -37,5 +41,5 @@ test("renders default image when image is not defined", ()=>{
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
-//2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use no more then 3 different expect statements to test the the existance of the summary value.
+//2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use no more then 3 different expect statements to test the the existence of the summary value.
 //3. The episode component displays a default value ('./stranger_things.png') when a image url is not provided. Create a new piece of test data with the image property set to null. Test that the alt tag of the image displayed is set to './stranger_things.png'.
