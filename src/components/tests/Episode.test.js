8e3f40d1a5db4a977674d9cd625f2a635b40ffe8
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-14 14:29:54 -0600

    updates to episode.test.js

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -14,19 +14,36 @@ const testEpisode = {
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
-}
+    id: 1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summeray: "",
+    runtime: 1,
+};
 
 test("renders without error", () => {
-
+    render(< Episode episode={testEpisode} />)
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode={testEpisode}/>);
+    const summary = screen.queryByText(/test summary/i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(/test summary/i);
+    expect(summary).not.toBeNull();
+
 });
 
 test("renders default image when image is not defined", ()=>{
-    
-})
+    render(<Episode episode={ testEpisodeWithoutImage } />);
+    const defaultImg = screen.getByAltText("./stranger_things.png");
+
+    expect(defaultImg).toBeInTheDocument();
+    expect(defaultImg).toHaveAttribute("alt", "./stranger_things.png");
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
