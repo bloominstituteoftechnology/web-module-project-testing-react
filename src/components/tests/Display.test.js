82c86c203c4a0e3c59efc009a81cafbe18542af7
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-14 16:32:56 -0600

    getting display.test set up

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { render, screen, waitFor } from  '@testing-library/react';
 import Display from '../Display';
-import fetchShow from '../../api/fetchShow';
+import mockFetchShow from '../../api/fetchShow';
 import userEvent from '@testing-library/user-event';
 jest.mock("../../api/fetchShow.js");
 
 
+const testShow = {
+    //add in approprate test data structure here.
+    name: "Stranger Things",
+    summary: "things aren't what they seem",
+    seasons: [
+        {
+            id: 0, name: "Season 1", episodes: [],
+        },
+        {
+            id: 1, name: "Season 2", episodes: []
+        },
+        {
+            id: 2, name: "Season 3", episodes: []
+        },
+        
+    ]
+}
+
+test('renders without errors', () => {
+    render(<Display/>);
+})
+
+test("when the fetch button is pressed, the show component will display", async () => {
+    render(<Display/>)
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    const showDetails = await screen.queryByTestId('show-container');
+    expect(showDetails).toBeInTheDocument();
+})
+
+test('when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data', async () => {
+    render(<Display />);
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    const button = screen.queryByRole('button');
+    userEvent.click(button);
+
+    const seasons = await screen.findAllByTestId('season-option');
+    expect(seasons).toHaveLength(3);
+})
 
 
 
