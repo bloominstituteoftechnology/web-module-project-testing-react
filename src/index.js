6a3c1efcb3284b8adab665e56e78624c7a2a236d
ferengitooth <ehitsamill@gmail.com>
2021-06-14 23:15:29 -0700

    installed dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
+// comment for initial commit
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
