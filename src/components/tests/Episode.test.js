9c1989f6d413b9c3dcca2ab962be9414d048f1a0
MurrayWarnock <murray.warnock@gmail.com>
2021-07-19 16:26:25 -0600

    Episode.test.js complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,27 +8,37 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "Text to test if correct content is passed",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "Text to test if correct content is passed",
+    runtime: 1
 }
 
-test("renders without error", () => {
-
-});
-
-test("renders the summury test passed as prop", ()=>{
-    
-});
-
-test("renders default image when image is not defined", ()=>{
-    
-})
-
-//Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+test("renders without error", () => {
+    render(<Episode episode={testEpisode}/>);
+});
 //2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use no more then 3 different expect statements to test the the existance of the summary value.
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>);
+    const summary = screen.queryByText(/text to test if correct content is passed/i)
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/text to test if correct content is passed/i);  
+});
 //3. The episode component displays a default value ('./stranger_things.png') when a image url is not provided. Create a new piece of test data with the image property set to null. Test that the alt tag of the image displayed is set to './stranger_things.png'.
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testEpisodeWithoutImage}/>);
+    const image = screen.getByAltText("./stranger_things.png");
+    expect(image).toBeInTheDocument();
+    expect(image).toBeTruthy();
+});
