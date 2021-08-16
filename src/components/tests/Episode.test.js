d93e934402120925e5a236b8556f3c9e5d0727e4
Bradford-Meziere <coleokc@gmail.com>
2021-08-16 16:50:01 -0500

    Episodes and Show testing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,36 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "This is a well written Episode.",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "This is a well written Episode.",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>)
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode} />)
+
+    const summary = screen.queryByText(/This is a well written Episode./i)
+    expect(summary).toHaveTextContent("This is a well written Episode.")
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>)
+
+    const image= screen.getByAltText('./stranger_things.png')
+    expect(image).toHaveAttribute("src", "./stranger_things.png")
 })
 
 //Tasks
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,28 +5,70 @@ import userEvent from '@testing-library/user-event';
 import Show from './../Show';
 
 const testShow = {
-    //add in approprate test data structure here.
+    name: "",
+    image: null,
+    summary: "",
+    seasons: [
+        {
+            id: 1,
+            name: "Season 1",
+            episodes: [],
+        },
+        {
+            id: 2,
+            name: "Season 2",
+            episodes: [],
+
+        }
+],
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} />)
+
+    const loading = screen.getByTestId("loading-container")
+    expect(loading).toBeInTheDocument()
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'} />)
+    const seasons = screen.getAllByTestId("season-option")
+    expect(seasons).toHaveLength(2)
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const fakeHandleSubmit = jest.fn()
+
+    render(<Show show={testShow} selectedSeason={'none'} handleSelect={fakeHandleSubmit}/>)
+
+    const options = screen.getByRole("combobox")
+
+    userEvent.selectOptions(options, "1")
+    expect(fakeHandleSubmit).toBeCalledTimes(1)
 });
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+test('component renders when no seasons are selected and then re-renders with a season passed in', () => {
+    const {rerender} = render(<Show selectedSeason={"none"} show={testShow}/>)
+
+    let episodes = screen.queryByTestId("episodes-container")
+    expect(episodes).not.toBeInTheDocument()
+
+    rerender(<Show show={testShow} selectedSeason={"1"}/>)
+    episodes = screen.queryByTestId("episodes-container")
+    expect(episodes).toBeInTheDocument()
 });
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
+
 //2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
+
 //3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
 //6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
