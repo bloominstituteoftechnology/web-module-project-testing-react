f67f83d1f407dcfa31ef8ae30fec654ce328f4be
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-08-17 03:58:12 -0400

    gg

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {
   fireEvent,
+  getAllByText,
   getByText,
   queryByRole,
+  queryByText,
   render,
   screen,
   userEvent,
@@ -15,35 +17,39 @@ import Display from "./components/Display";
 import { fetchEpisodes as mockFetchEpisodes } from "./api/fetchShow";
 import fetchShow from "./api/fetchShow";
 import Episodes from "./components/Episodes";
-test("target dropdown", async () => {
-  render(<App />);
-
-  await waitFor(() => {
-    const placeholder = screen.queryAllByPlaceholderText("Select an option");
-    console.log(placeholder);
-  });
-});
+import Show from "./components/Show";
 describe("Test runner!", () => {
-  test("App Renders", () => {
+  test("target dropdown", async () => {
     render(<App />);
+
+    await waitFor(() => {
+      const placeholder = screen.queryAllByPlaceholderText("Select an option");
+      console.log(placeholder);
+    });
   });
-  test("Display renders", () => {
-    render(<Display />);
+  test("The Episode Component", async () => {
+    const rW = render(<Display />);
+    const img = rW.getByRole("img");
+    rW.debug(img);
+    expect(img.getAttribute("alt")).toEqual("./stranger_things.png");
+    return render(img.setAttribute("src", null));
   });
-});
-describe("Test Episode", () => {
-  const testData = {
-    id: 0,
-    image: null,
-    name: "Testing Things",
-    season: 1,
-    number: 1,
-    summary: "Testing Things",
-    runtime: 60,
+  const testShow = {
+    selectedSeason: "none",
+    show: {
+      name: "Testing Things",
+      summary: "Testing Things is about running test!",
+      seasons: [
+        { id: 0, name: "Testing Vol 1", episodes: [] },
+        { episodes: [], id: 1, name: "Testing Vol 2" },
+        { episodes: [], id: 2, name: "Testing Vol 3" },
+        { episodes: [], id: 3, name: "Testing Vol 4" },
+        { episodes: [], id: 4, name: "Testing Vol 5" },
+      ],
+    },
   };
-  //   render(<Episode episode={testData} />);
-  //   const imgTarget = screen.queryByRole("img");
-  //   expect(imgTarget.getAttribute("src")).toBe("./stranger_things.png");
-  //   expect(imgTarget.getAttribute("alt")).toBe("./stranger_things.png");
-  render(<App />);
+  test("The Show Component", () => {
+    console.log(testShow);
+    const testing = render(<Show props={testShow} />);
+  });
 });
