75422ec7f41578a8a31b254f44775920ea0d8a52
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-08-17 02:12:25 -0400

    Start

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import { render, screen, userEvent, waitFor } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  fireEvent,
+  getByText,
+  queryByRole,
+  render,
+  screen,
+  userEvent,
+  waitFor,
+  wrapper,
+} from "@testing-library/react";
+import App from "./App";
+import Episode from "./components/Episode";
+import Display from "./components/Display";
+import { fetchEpisodes as mockFetchEpisodes } from "./api/fetchShow";
+import fetchShow from "./api/fetchShow";
+import Episodes from "./components/Episodes";
+test("target dropdown", async () => {
+  render(<App />);
 
-test('target dropdown', async ()=>{
-    render(<App/>);
-
-    await waitFor(()=>{
-        const placeholder = screen.queryAllByPlaceholderText('Select an option');
-        console.log(placeholder);
-    });
-    
+  await waitFor(() => {
+    const placeholder = screen.queryAllByPlaceholderText("Select an option");
+    console.log(placeholder);
+  });
+});
+describe("Test runner!", () => {
+  test("App Renders", () => {
+    render(<App />);
+  });
+  test("Display renders", () => {
+    render(<Display />);
+  });
+});
+describe("Test Episode", () => {
+  const testData = {
+    id: 0,
+    image: null,
+    name: "Testing Things",
+    season: 1,
+    number: 1,
+    summary: "Testing Things",
+    runtime: 60,
+  };
+  //   render(<Episode episode={testData} />);
+  //   const imgTarget = screen.queryByRole("img");
+  //   expect(imgTarget.getAttribute("src")).toBe("./stranger_things.png");
+  //   expect(imgTarget.getAttribute("alt")).toBe("./stranger_things.png");
+  render(<App />);
 });
