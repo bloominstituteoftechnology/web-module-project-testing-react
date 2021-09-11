4f1bd9c518fc511db292fe5e14ca5e17469b12fe
unknown <j.ordonez419@gmail.com>
2021-09-10 21:05:38 -0500

    MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render, screen, userEvent, waitFor } from '@testing-library/react';
 import App from './App';
+import Episode from './components/Episode'
 
-test('target dropdown', async ()=>{
-    render(<App/>);
+test('target dropdown', async () => {
+    render(<App />);
 
-    await waitFor(()=>{
+    await waitFor(() => {
         const placeholder = screen.queryAllByPlaceholderText('Select an option');
         console.log(placeholder);
     });
-    
+
 });
+
+test('that episode component renders, providing it example episode data as a prop', () => {
+    render(<Episode />);
+    let title = screen.queryByText(/space missions/i);
+    expect(title).not.toBeNull();
+})
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 const testEpisode = {
-    id:1,
+    id: 1,
     name: "",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
@@ -14,18 +14,35 @@ const testEpisode = {
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id: 2,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "wiithout image",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={[]} />)
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", () => {
+    render(<Episode episode={testEpisode} />)
+
+    const summary = screen.getByText(/summary/i);
+
+    expect(summary).toBeTruthy();
+    expect(summary).toBeInTheDocument();
+    expect(summary).toHaveTextContent(testEpisode.summary);
 });
 
-test("renders default image when image is not defined", ()=>{
-    
+test("renders default image when image is not defined", () => {
+    const image = screen.getByAltText("./stranger_things.png");
+
+    //Image should not be in the doc
+    expect(image).toHaveAttribute("alt", "./stranger_things.png");
+    expect(image).toBeTruthy();
 })
 
 //Tasks
