d123ec396c4a75e24d7ae6c616b16748831be43f
Richard Oh <n24512@protonmail.com>
2021-09-13 15:51:41 -0500

    Episode.test.js complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,32 +1,54 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
 const testEpisode = {
-    id:1,
-    name: "",
-    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    season: 1,
-    number: 1,
-    summary: "",
-    runtime: 1
-}
+  id: 1,
+  name: "",
+  image:
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+  season: 1,
+  number: 1,
+  summary: "",
+  runtime: 1,
+};
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+  //Add in appropriate test data structure here.
+  id: 1,
+  name: "",
+  image: null,
+  season: 1,
+  number: 1,
+  summary: "",
+  runtime: 1,
+};
 
 test("renders without error", () => {
-
+  render(<Episode />);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
+
+  const seasonEpisode = screen.queryByText(/season 1, episode 1/i);
+  const runTime = screen.queryByText(/1 minutes/i);
+  const image = screen.queryByAltText(
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg"
+  );
+
+  expect(seasonEpisode).toBeInTheDocument();
+  expect(runTime).toBeInTheDocument();
+  expect(image).toBeTruthy();
 });
 
-test("renders default image when image is not defined", ()=>{
-    
-})
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisodeWithoutImage} />);
+
+  const image = screen.queryByAltText("./stranger_things.png");
+
+  expect(image).toBeInTheDocument();
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,27 +1,71 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  //add in appropriate test data structure here.
+  name: "Test Series",
+  summary: "A test summary",
+  image: {
+    medium:
+      "https://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg",
+    original:
+      "https://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg",
+  },
+  seasons: [
+    {
+      id: 0,
+      name: "Test 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "Test 2",
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: "Test 3",
+      episodes: [],
+    },
+    {
+      id: 3,
+      name: "Test 4",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+
+  const name = screen.queryByText(/test series/i);
+  const summary = screen.queryByText(/a test summary/i);
+  //   const showContainer = screen.queryByTestId("show-container");
+  //   const option = screen.queryByLabelText(/select a season/i);
+  //   userEvent.selectOptions(option, ["none"]);
+
+  expect(name).toBeInTheDocument();
+  expect(summary).toBeInTheDocument();
 });
 
-test('renders Loading component when prop show is null', () => {
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} selectedSeason={"none"} />);
+
+  const loadingContainer = screen.queryByTestId("loading-container");
+  const loadingString = screen.queryByText(/fetching data.../i);
+
+  expect(loadingContainer).toBeInTheDocument();
+  expect(loadingString).toBeInTheDocument();
 });
 
-test('renders same number of options seasons are passed in', ()=>{
-});
+test("renders same number of options seasons are passed in", () => {});
 
-test('handleSelect is called when an season is selected', () => {
-});
+test("handleSelect is called when an season is selected", () => {});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
-});
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
