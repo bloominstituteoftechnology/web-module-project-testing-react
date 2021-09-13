86e91fcf20e520c5d6cbaf7e5214cfefb8c189e9
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-13 07:25:42 -0700

    Initial commit

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -14,7 +14,7 @@
 
 
 ///Tasks:
-//1. Add in nessisary imports and values to establish the testing suite.
+//1. Add in necessary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
