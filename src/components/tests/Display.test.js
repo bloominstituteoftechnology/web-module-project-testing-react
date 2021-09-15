0129f8ea4d44b771d3af901e30adae6c724a61f1
Sebastian <sebas6292@gmail.com>
2021-09-15 14:47:47 -0700

    done

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,16 +1,69 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
+import Display from './../Display';
 
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
 
+const testShow = {
+    name: 'test show',
+    summary: 'test summary', 
+    seasons: [
+        {
+            id: 0, 
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1, 
+            name: 'Season 2',
+           episodes: []
+        },
+    ]
+}
 
+test('renders without errors', ()=>{
+    render(<Display />); 
+})
 
+test('renders Show component when the button is clicked', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
 
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+})
 
+test('renders seasons options matching fetch return when button is clicked', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
 
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
+    await waitFor(()=>{
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        // console.log(seasonOptions);
+        expect(seasonOptions).toHaveLength(2);
+    })
+})
 
+test('displayFunc is called when the fetch button is pressed', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    const displayFunc = jest.fn();
 
-
+    render(<Display displayFunc={displayFunc}/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    await waitFor(()=>{
+        expect(displayFunc).toHaveBeenCalled();
+    })
+})
 
 
 ///Tasks:
