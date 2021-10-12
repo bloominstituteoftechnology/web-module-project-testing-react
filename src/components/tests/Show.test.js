dd2de4c104986da062936b333363dbc1eb4f30b2
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-11 18:15:25 -0700

    still working on show component. issue is the episodes prop isnt being read properly..

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -4,11 +4,37 @@ import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
 
+const selectedSeason = "Season 1"
 const testShow = {
-    //add in approprate test data structure here.
+    name: "Stranger Thinks",
+    summary: "Strange... very strange.",
+    seasons: [
+        {
+            id: 1,
+            name: "Season 1",
+            episodes: [
+                {
+                    airdate: "2016-07-15",
+                    airstamp: "2016-07-15T12:00:00+00:00",
+                    airtime: "",
+                    id: 553946,
+                    image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
+                    name: "Chapter One: The Vanishing of Will Byers",
+                    number: 1,
+                    runtime: 49,
+                    season: 1,
+                    summary: "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+                    type: "regular",
+                    url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers"
+                }
+            ]
+        }
+    ]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    console.log(testShow.seasons[0].episodes);
+    render(<Show show={testShow} episodes={testShow.seasons['Season 1'].episodes} />);
 });
 
 test('renders Loading component when prop show is null', () => {
