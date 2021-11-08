429aee1b7c57d997ca4c89fad3707fb1a8539610
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-08 16:31:16 -0500

    Added Episode.test

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,6 @@
 
-
+test("renders without error", ()=>{
+})
 
 
 
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,41 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "summary statement",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
     //Add in approprate test data structure here.
+    id:2,
+    name: "",
+    image: null,
+    season: 2,
+    number: 2,
+    summary: "",
+    runtime: 2
 }
 
 test("renders without error", () => {
-
+    // return(<Episode episode = {testEpisode}/>);
+    render(<Episode episode={testEpisode} />);
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode = {testEpisode} />);
+
+    const testData = screen.queryByText(/summary/i);
+    expect(testData).toBeTruthy();
+    expect(testData).toBeInTheDocument();
+    expect(testData).toHaveTextContent('summary statement');
+
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode = {testEpisodeWithoutImage} />);
+
+    const altTag = screen.queryByAltText("./stranger_things.png");
+    expect(altTag).toBeInTheDocument();
 })
 
 //Tasks
