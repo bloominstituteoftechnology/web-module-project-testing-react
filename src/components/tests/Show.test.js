ab25b99e6ba40aa1241dc528ef300a578a57f6b4
Angelique Abacajan <ma_utsey@yahoo.com>
2021-11-08 16:08:29 -0500

    add in appropriate test data structure

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,9 +3,39 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Episode from '../Episode';
+import Loading from '../Loading';
 
 const testShow = {
-    //add in approprate test data structure here.
+    //add in appropriate test data structure here.
+    name: "Stranger Things",
+    img: "../../../public/stranger_things.png",
+    summary: "Movie summary: After the mysterious and sudden vanishing of a young boy, the people of a small town begin to uncover secrets of a government lab, portals to another world and sinister monsters. The boy's mother (Joyce) desperately tries to find him, convinced he is in grave danger, while the police chief searches for answers",
+    seasons: [
+        // season 1
+        {   id: 0,
+            name: "Season 1",
+            episodes: []
+        },
+
+        // season 2
+        {   id: 1,
+            name: "Season 2",
+            episodes: []
+        },
+
+        // season 3
+        {   id: 2,
+            name: "Season 3",
+            episodes: []
+        },
+
+        // season 4 
+        {   id: 3,
+            name: "Season 4",
+            episodes: []
+        }
+    ]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
