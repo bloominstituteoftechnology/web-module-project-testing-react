3dde62086f7c575bb057c916828c90995f057453
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-11-08 17:15:39 -0600

    done

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,68 @@
+import React from "react";
+import { render, screen, waitFor} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
+import Display from './../Display' 
+import mockFetchShow from './../../api/fetchShow'
+jest.mock('./../../api/fetchShow')
 
+const testShow = {
+    
+    name: "test show",
+    summary: "test summary",
+    seasons:[
+        {
+            id: 0,
+            name: "Season 1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "Season 2",
+            episodes: []
+        }
+    ]
+};
+ test('renders without errors', ()=>{
+render(<Display/>);
+ });
 
+ test('renders Show Component', async ()=>{
+     mockFetchShow.mockResolvedValueOnce(testShow);
 
+    render(<Display/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+});
 
+test('renders season options', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
 
+   render(<Display/>);
+   const button = screen.getByRole('button');
+   userEvent.click(button);
+
+   await waitFor(()=>{
+       const seasonOptions = screen.queryAllByTestId('season-option');
+       expect(seasonOptions).toHaveLength(2)
+   })
+});
+
+test('displayFunc', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+   const displayFunc = jest.fn();
+   render(<Display displayFunc={displayFunc}/>);
+   const button = screen.getByRole('button');
+   userEvent.click(button);
+
+   await waitFor(()=>{
+      
+       expect(displayFunc).toHaveBeenCalled();
+   })
+});
 
 
 
