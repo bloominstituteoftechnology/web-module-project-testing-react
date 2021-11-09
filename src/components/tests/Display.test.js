14187f940c01eda1ed2ca57b0141566514d1ba4e
djho57 <daniel.j.ho57@gmail.com>
2021-11-08 23:52:52 -0600

    mvp

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,82 @@
+import React from "react";
+import Display from "./../Display";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+import mockFetchShow from "./../../api/fetchShow";
 
+jest.mock("./../../api/fetchShow");
 
+const testShow = {
+    name: "Stranger Things",
+    img: "../../../public/stranger_things.png",
+    summary: "Movie summary: After the mysterious and sudden vanishing of a young boy, the people of a small town begin to uncover secrets of a government lab, portals to another world and sinister monsters. The boy's mother (Joyce) desperately tries to find him, convinced he is in grave danger, while the police chief searches for answers",
+    seasons: [
+        // season 1
+        {   id: 0,
+            name: "Season 1",
+            episodes: []
+        },
 
+        // season 2
+        {   id: 1,
+            name: "Season 2",
+            episodes: []
+        },
 
+        // season 3
+        {   id: 2,
+            name: "Season 3",
+            episodes: []
+        },
 
+        // season 4 
+        {   id: 3,
+            name: "Season 4",
+            episodes: []
+        }
+    ]
+};
 
+test("render without errors", () => {
+  render(<Display />);
+});
 
+test("renders Show component when the button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
 
+  const show = await screen.findByTestId("show-container");
 
+  expect(show).toBeInTheDocument();
+});
 
+test("renders season options matching fetch returns when button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
 
+  await waitFor(() => {
+    const seasonOptions = screen.queryAllByTestId("season-option");
+    expect(seasonOptions).toHaveLength(2);
+  });
+});
 
+test("displayFunc is called when the fetch button is pressed", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  const displayFunc = jest.fn();
 
+  render(<Display displayFunc={displayFunc} />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    expect(displayFunc).toHaveBeenCalled();
+  });
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,40 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "Movie summary: After the mysterious and sudden vanishing of a young boy, the people of a small town begin to uncover secrets of a government lab, portals to another world and sinister monsters. The boy's mother (Joyce) desperately tries to find him, convinced he is in grave danger, while the police chief searches for answers",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id: 1,
+    name: "",
+    image: "",
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode = { testEpisode } />);
 });
 
 test("renders the summury test passed as prop", ()=>{
-    
+    render(<Episode episode = { testEpisode } />);
+
+    const summary = screen.queryByText(/ test /i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent("Test Summary");
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode = { testEpisode } />);
+
+    const img = screen.queryByAltText("./stranger_things.png");
+
+    expect(img).toBeInTheDocument();
 })
 
 //Tasks
