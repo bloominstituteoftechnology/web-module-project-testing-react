93e7319cc1095b4b2ef0438bd45cad921b44d72f
Hughes <hughespham2002the2nd@yahoo.com>
2021-11-08 22:08:39 -0600

    mvp

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,24 +3,82 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Episode from '../Episode';
+import Loading from '../Loading';
 
 const testShow = {
-    //add in approprate test data structure here.
+    name: "Stranger Things",
+    img: "../../../public/stranger_things.png",
+    summary: "Movie summary: After the mysterious and sudden vanishing of a young boy, the people of a small town begin to uncover secrets of a government lab, portals to another world and sinister monsters. The boy's mother (Joyce) desperately tries to find him, convinced he is in grave danger, while the police chief searches for answers",
+    seasons: [
+        // season 1
+        {   id: 0,
+            name: "Season 1",
+            episodes: []
+        },
+
+        // season 2
+        {   id: 1,
+            name: "Season 2",
+            episodes: []
+        },
+
+        // season 3
+        {   id: 2,
+            name: "Season 3",
+            episodes: []
+        },
+
+        // season 4 
+        {   id: 3,
+            name: "Season 4",
+            episodes: []
+        }
+    ]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render( <Show testShow = { testShow } selectedSeason = { "none" }/>);
 });
 
 test('renders Loading component when prop show is null', () => {
+    render( <Show show = { null } />);
+
+    const loading = screen.queryByTestId("loading-container");
+
+    expect(loading).toBeInTheDocument();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+    render( <Show show = { testShow } />);
+
+    const seasonsOpt = testShow.seasons;
+
+    expect(seasonsOpt).toHaveLength(4);
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    render( <Show show = { testShow } selectedSeason = { "none" } handleSelect = { handleSelect }/>);
+
+    const option = screen.getByLabelText( /Select A Season /i);
+
+    userEvent.selectOptions(option, ["3"]);
+
+    expect(handleSelect).toBeCalled();
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render( <Show testShow = { testShow } selectedSeason = { "none" }/>);
+
+    const episodes = screen.queryAllByTestId("episodes-container");
+
+    expect(episodes).toBeFalsy();
+
+    rerender( <Show testShow = { testShow } selectedSeason = { 1 } />);
+    episodes = screen.queryByTestId("episodes-container");
+
+    expect(episodes).toBeTruthy();
 });
 
 //Tasks:
