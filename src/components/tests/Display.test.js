74ffb6194bd7d822badd5ab28386ab2ac1407236
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-11 19:20:01 -0800

    Display.test.js - Test that the Display component renders without any passed in props.

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Display from './../Display'
 
 test ("render without errors", () => {
-    
+    render (<Display />);
 })
+
+
+
+
+
+
+///Tasks:
+//1. Add in nessisary imports and values to establish the testing suite.
+//2. Test that the Display component renders without any passed in props.
+//3. Rebuild or copy a show test data element as used in the previous set of tests.
+//4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
+//5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
+//6. Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
