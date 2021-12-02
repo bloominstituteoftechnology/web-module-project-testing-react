511d8b705c713b89135da4f459bed5af64ee203a
wlongmireBloomTech <71340015+wlongmireBloomTech@users.noreply.github.com>
2021-12-02 17:45:48 -0500

    Update Display.test.js

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,67 +5,13 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
-import mockFetchShow from './../../api/fetchShow';
-jest.mock('./../../api/fetchShow');
 
-const testShow = {
-    name: "test name",
-    summary: "test summary",
-    seasons:[
-        {
-            id: 0,
-            name: "season 1",
-            episodes:[]
-        },
-        {
-            id: 1,
-            name: "season 2",
-            episodes:[]
-        }
-    ],
-};
+test('renders without errors with no props', ()=>{
 
-
-test('renders without errors with no props', async ()=>{
-    render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', async ()=>{
-    mockFetchShow.mockResolvedValueOnce(testShow);
+test('renders Show component when the button is clicked ', ()=>{});
 
-    render(<Display/>);
-    
-    const button = screen.getByRole("button");
-    userEvent.click(button);
+test('renders show season options matching your data when the button is clicked', ()=>{});
 
-    const show = await screen.findByTestId('show-container');
-
-    expect(show).toBeInTheDocument();
-});
-
-test('renders show season options matching your data when the button is clicked', async ()=>{
-    mockFetchShow.mockResolvedValueOnce(testShow);
-
-    render(<Display/>);
-    
-    const button = screen.getByRole("button");
-    userEvent.click(button);
-    
-    const seasonOptions = await screen.findAllByTestId('season-option');
-    
-    expect(seasonOptions).toHaveLength(2);
-});
-
-test('renders show season options matching your data when the button is clicked', async ()=>{
-    mockFetchShow.mockResolvedValueOnce(testShow);
-    const displayFunc = jest.fn();
-
-    render(<Display displayFunc={displayFunc}/>);
-    
-    const button = screen.getByRole("button");
-    userEvent.click(button);
-    
-    await waitFor(()=> {
-        expect(displayFunc).toHaveBeenCalled();
-    })
-});
+test('renders show season options matching your data when the button is clicked', ()=>{});
