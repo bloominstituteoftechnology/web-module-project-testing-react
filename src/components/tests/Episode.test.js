b6369891e80e9780d33cdded9ce9122c5355a05d
Aaron Belmore <belmoreaaron9@gmail.com>
2023-06-23 19:18:44 -0400

    COmpleted Episode, and Show test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,26 +3,61 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  render(<Episode episode={exampleEpisodeData} />);
+});
 
-test("renders the summary test passed as prop", () => {});
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={exampleEpisodeData} />);
+  const summaryData = screen.getByText(exampleEpisodeData.summary);
 
-test("renders default image when image is not defined", () => {});
+  expect(summaryData).toBeInTheDocument();
+  expect(summaryData).toBeTruthy();
+  expect(summaryData).toHaveTextContent(exampleEpisodeData.summary);
+});
+
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={nullImgData} />);
+  const imgData = screen.getByAltText(
+    "https://i.ibb.co/2FsfXqM/stranger-things.png"
+  );
+
+  expect(imgData).toBeTruthy();
+  expect(imgData).toBeVisible();
+  expect(imgData).not.toBeNull();
+});
 
 // ----- EXAMPLE EPISODE TEST OBJECT -----
-// const exampleEpisodeData = {
-//   airdate: "2016-07-15",
-//   airstamp: "2016-07-15T12:00:00+00:00",
-//   airtime: "",
-//   id: 553946,
-//   image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
-//   name: "Chapter One: The Vanishing of Will Byers",
-//   number: 1,
-//   rating: { average: 8.2 },
-//   runtime: 49,
-//   season: 1,
-//   summary:
-//     "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
-//   type: "regular",
-//   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-// };
+const exampleEpisodeData = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  image:
+    "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
+  name: "Chapter One: The Vanishing of Will Byers",
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+};
+const nullImgData = {
+  airdate: "2016-07-15",
+  airstamp: "2016-07-15T12:00:00+00:00",
+  airtime: "",
+  id: 553946,
+  image: null,
+  name: "Chapter One: The Vanishing of Will Byers",
+  number: 1,
+  rating: { average: 8.2 },
+  runtime: 49,
+  season: 1,
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+  type: "regular",
+  url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+};
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Show from './../Show';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Show from "./../Show";
+import userEvent from "@testing-library/user-event";
 
-test('renders without errors', () => { });
+const testShow = {
+  name: "test show",
+  summary: "insert here",
+  seasons: [
+    { id: 0, name: "Season 1", episodes: [] },
+    { id: 1, name: "Season 2", episodes: [] },
+  ],
+};
+test("renders without errors", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+});
 
-test('renders Loading component when prop show is null', () => { });
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} />);
+  const loadingDisplay = screen.queryByTestId("loading-container");
 
-test('renders same number of options seasons are passed in', () => { });
+  expect(loadingDisplay).toBeTruthy();
+  expect(loadingDisplay).toBeInTheDocument();
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={testShow} selectedSeason={"none"} />);
+  const seasons = screen.queryAllByTestId("season-option");
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+  expect(seasons).toHaveLength(2);
+});
+
+test("handleSelect is called when an season is selected", () => {
+  const handleSelect = jest.fn();
+  render(
+    <Show show={testShow} selectedSeason={"none"} handleSelect={handleSelect} />
+  );
+  const selectSeason = screen.getByLabelText(/select a season/i);
+
+  fireEvent.change(selectSeason, { target: { value: 1 } });
+
+  expect(handleSelect).toBeCalled();
+});
+
+test("component renders when no seasons are selected and when rerenders with a season passed in", () => {
+  const { rerender } = render(<Show show={testShow} selectedSeason={"none"} />);
+  let episodes = screen.queryByTestId("episodes-container");
+  expect(episodes).not.toBeInTheDocument();
+
+  rerender(<Show show={testShow} selectedSeason={1} />);
+  episodes = screen.queryByTestId("episodes-container");
+  expect(episodes).toBeInTheDocument();
+});
+
+// Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render
+// the episode component when the selectedSeason prop has a valid season index.
