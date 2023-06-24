51691db31ce572d97340ad7f17854b2f10593d2c
Aaron Belmore <belmoreaaron9@gmail.com>
2023-06-23 21:09:57 -0400

    Completed proj MVP

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Display from "./../Display";
 import userEvent from "@testing-library/user-event";
@@ -19,11 +19,24 @@ test("renders without errors with no props", async () => {
   render(<Display />);
 });
 
-test("renders Show component when the button is clicked ", () => {
+test("renders Show component when the button is clicked ", async () => {
   mockFetchShow.mockResolvedValueOnce(testShow);
   render(<Display />);
   const button = screen.getByRole("button");
   userEvent.click(button);
+
+  const show = await screen.findByTestId("show-container");
+  expect(show).toBeInTheDocument();
 });
 
-test("renders show season options matching your data when the button is clicked", () => {});
+test("renders show season options matching your data when the button is clicked", async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow);
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const seasons = screen.findAllByTestId("season-option");
+    console.log(seasons);
+  });
+});
